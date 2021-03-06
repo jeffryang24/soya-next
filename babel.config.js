4d442ecfab212d2b1390f11d78d8544a2c9ab539
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-03-07 00:07:37 +0700

    refactor(babel-config): enable esmodule when babelEnv is esm

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,25 @@
 "use strict";
 
-module.exports = {
-  presets: ["next/babel"]
+module.exports = function babelConfig(api) {
+  api.cache(true);
+
+  const presetEnvOptions =
+    process.env.BABEL_ENV === "esm"
+      ? {
+          modules: false,
+        }
+      : {};
+
+  return {
+    presets: [
+      [
+        "next/babel",
+        {
+          "preset-env": presetEnvOptions,
+        },
+      ],
+    ],
+    comments: false,
+    ignore: ["**/__tests__/**"],
+  };
 };

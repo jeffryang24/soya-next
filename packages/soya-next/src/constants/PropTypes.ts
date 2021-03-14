2056514e296d203ad4e4a9038c3b96ac2e733185
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-03-14 14:44:23 +0700

    docs(prop-types): add locale propTypes doc

--- packages/soya-next/src/constants/PropTypes.ts
+++ packages/soya-next/src/constants/PropTypes.ts
@@ -1,5 +1,12 @@
 import PropTypes from "prop-types";
 
+/**
+ * Soya Next Locale PropTypes Shape
+ * ---
+ * If you are using typescript in your project,
+ * probably you may use `SoyaNextLocale` interface
+ * from `soya-next` package.
+ */
 export const localeShape = PropTypes.shape({
   country: PropTypes.string,
   language: PropTypes.string,

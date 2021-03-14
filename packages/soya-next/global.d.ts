b45f862ede137f24f52e0cf31958e6f365d1630f
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-03-14 20:36:15 +0700

    feat(global_dts): add new global types

--- packages/soya-next/global.d.ts
+++ packages/soya-next/global.d.ts
@@ -1,5 +1,31 @@
-// INFO: status & statusCode fields are used inside decodeParam util.
-declare interface URIError extends Error {
-  status?: number;
-  statusCode?: number;
+import type { NEXT_DATA } from "next/dist/next-server/lib/utils";
+
+declare global {
+  declare var __NEXT_DATA__: NEXT_DATA;
+
+  // INFO: status & statusCode fields are used inside decodeParam util.
+  declare interface URIError extends Error {
+    status?: number;
+    statusCode?: number;
+  }
+
+  // Extend the NodeJS namespace with Next.js-defined properties
+  declare namespace NodeJS {
+    // INFO: Not set it to readonly for testing purpose.
+    interface Process {
+      browser: boolean;
+    }
+
+    interface ProcessEnv {
+      readonly NODE_ENV: "development" | "production" | "test";
+    }
+  }
+}
+
+declare module "next/dist/next-server/lib/utils" {
+  declare interface NextPageContext {
+    defaultLocale?: string;
+    siteLocales?: string[];
+    locale?: SoyaNextLocale;
+  }
 }
